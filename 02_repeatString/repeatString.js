1400c7212c28803991061235a635f42be474717c
MedesPersians <156643231+MedesPersians@users.noreply.github.com>
2025-04-01 12:10:28 +0100

    Solution for repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    let i = 1;
+    let newString = '';
+    while (i <=number) {
+        newString += string
+        // console.log(string);
+        i++;
+    ;}
+    return newString;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
